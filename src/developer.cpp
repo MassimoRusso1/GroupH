f636f67f1d04a12c616c5182da4a9f1a4774b241
Nico <122835168+tsarocin@users.noreply.github.com>
2023-06-02 20:59:36 +0000

    created the constructors and implemented the solve_problem and drink_coffee functions.

--- src/developer.cpp
+++ src/developer.cpp
@@ -1,5 +1,42 @@
 #include "developer.hpp"
+#include <iostream>
+#include <string>
+
+Developer::Developer(const std::string& name, const std::string& alias) : name_{name}, alias_{alias}
+{
+}
+
+SeniorDeveloper::SeniorDeveloper(const std::string& name, const std::string& alias) : Developer(name, alias)
+{
+}
+
+JuniorDeveloper::JuniorDeveloper(const std::string& name, const std::string& alias) : Developer(name, alias)
+{
+}
+
+void Developer::drink_coffee()
+{
+    std::cout<<"I need an esspresso!" <<std::endl;
+}
 
 void SeniorDeveloper::solve_problem() const
 {
+    std::cout <<*this << std::endl;
+    std::cout << "That was sooo easy!!!"
+    drink_coffee();
 }
+
+void JuniorDeveloper::solve_problem() const
+{
+    std::cout <<*this << std::endl;
+    std::cout << "I nearly died!!!"
+    drink_coffee();
+}
+
+auto operator<<(std::ostream& out, const Developer& dev) -> std::ostream&
+{
+    out <<"Name: " <<dev.get_name()<< "\n"
+}
+
+
+
