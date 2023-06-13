dfa0f131f4a3ceb5801d5ad9b8bef73df83d21bb
MassimoRusso1 <122785115+MassimoRusso1@users.noreply.github.com>
2023-06-13 17:04:46 +0000

    added dynamic pointer cast to make sure the derived classes can use the shared pointer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
+#include <developer.hpp>
 #include <iostream>
 #include <memory>
 #include <vector>
 
-#include "developer.hpp"
-
 auto main() -> int
 {
     std::vector<std::shared_ptr<Developer>> developers;
-    std::shared_ptr<SeniorDeveloper> senior_developer =std::make_shared<SeniorDeveloper>("Massimo Russo", "MassimoRusso1");
+    std::shared_ptr<SeniorDeveloper> senior_developer = std::make_shared<SeniorDeveloper>("Massimo Russo", "MassimoRusso1");
     std::shared_ptr<JuniorDeveloper> junior_developer = std::make_shared<JuniorDeveloper>("Nico Rast", "tsarocin");
-    
 
-    developers.push_back(junior_developer);
-    developers.push_back(senior_developer);
+    developers.push_back(std::dynamic_pointer_cast<Developer>(junior_developer));
+    developers.push_back(std::dynamic_pointer_cast<Developer>(senior_developer));
 
     for (const auto& itr : developers)
     {
