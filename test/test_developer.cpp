711dacfae9d9140aed83c87566ebbec6af1f5717
Nico <122835168+tsarocin@users.noreply.github.com>
2023-06-16 14:55:13 +0000

    commited the test_file

--- test/test_developer.cpp
+++ test/test_developer.cpp
@@ -1,5 +1,5 @@
+//include statements for the Google Test framework
 #include <gtest/gtest.h>
-
 #include <developer.hpp>
 
 class DeveloperTestSuite : public ::testing::Test
@@ -10,14 +10,20 @@ class DeveloperTestSuite : public ::testing::Test
 
 };
 
+//Test case for SeniorDeveloper constructor
 TEST_F(DeveloperTestSuite, SeniorDeveloperConstructor)
 {
+    //Check if the name is correctly set
     EXPECT_EQ("Test Senior", senior_developer_.get_name());
+    //Check if the alias is correctly set
     EXPECT_EQ("Senior",senior_developer_.get_alias());
 }
 
+//Test case for JuniorDeveloper constructor
 TEST_F(DeveloperTestSuite, JuniorDeveloperConstructor)
 {
+    //Check if the name is correctly set
     EXPECT_EQ("Test Junior", junior_developer_.get_name());
+    //Check if the alias is correctly set
     EXPECT_EQ("Junior", junior_developer_.get_alias());
 }
